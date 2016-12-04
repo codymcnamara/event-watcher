098e3211a536c6253c9c5f5735c1a6b7067ce401
Cody McNamara <codyjamesmcnamara@gmail.com>
2016-12-04 15:41:06 -0800

    front end boilerplate for events page and add d3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'events/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -13,6 +15,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :events
 
   # Example resource route with options:
   #   resources :products do
@@ -53,4 +56,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+
 end
