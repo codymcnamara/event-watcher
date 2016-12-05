a2699f94f37a4a5f8e7a578c994c1d12f65d004c
Cody McNamara <codyjamesmcnamara@gmail.com>
2016-12-04 21:34:07 -0800

    inital javascript to display graph axes

---
+++ app/assets/javascripts/graph.js
@@ -0,0 +1,58 @@
+
+graphFunctions = {
+  init: function () {
+    $.ajax({
+      url: window.location.pathname + '.json',
+      type: 'GET',
+      success: function (data) {
+        graphFunctions.createGraph(data);
+      }
+    })
+  },
+
+  createGraph: function (lineData) {
+    var vis = d3.select('#graph'),
+        WIDTH = 1000,
+        HEIGHT = 500,
+        MARGINS = {
+          top: 20,
+          right: 20,
+          bottom: 50,
+          left: 50
+        },
+        xRange = d3.scaleTime()
+          .range([MARGINS.left, WIDTH - MARGINS.right])
+          .domain([
+            new Date( Date.parse(lineData[0]) ),
+            new Date( Date.parse(lineData[lineData.length - 1]) )
+          ]),
+        yRange = d3.scaleLinear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([d3.min(lineData, function(d) {
+          return d.lowest_price;
+        }), d3.max(lineData, function(d) {
+          return d.lowest_price;
+        })]),
+        xAxis = d3.axisBottom(xRange)
+          .ticks( Math.round((lineData.length - 1)/2) ),
+          // .tickSize(5)
+          // .tickSubdivide(true),
+        yAxis = d3.axisLeft()
+          .scale(yRange)
+          .tickSize(5);
+
+    vis.append('svg:g')
+      .attr('class', 'x axis')
+      .attr('transform', 'translate(0,' + (HEIGHT - MARGINS.bottom) + ')')
+      .call(xAxis);
+
+    vis.append('svg:g')
+      .attr('class', 'y axis')
+      .attr('transform', 'translate(' + (MARGINS.left) + ',0)')
+      .call(yAxis);
+  }
+
+}
+
+
+$(document).ready(function() {
+  graphFunctions.init();
+})
