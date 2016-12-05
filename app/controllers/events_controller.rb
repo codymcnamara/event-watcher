1e5425d25dc90797bf3ef65b9be99fedc604a67f
Cody McNamara <codyjamesmcnamara@gmail.com>
2016-12-04 16:29:28 -0800

    add json endpoint returning an events days

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,13 @@
 class EventsController < ApplicationController
+
   def show
+    @event = Event.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json {
+        render :json => @event.days
+      }
+    end
   end
+
 end
