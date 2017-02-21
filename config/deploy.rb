9e311a3ce6ade6ebb07f1c67a48010af36f79167
Cody McNamara <codyjamesmcnamara@gmail.com>
2017-02-20 19:19:43 -0800

    move whenver capistrano plugin from deploy.rb to capfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,8 +27,6 @@ set :puma_worker_timeout, nil
 set :puma_init_active_record, true
 set :puma_preload_app, false
 
-require "whenever/capistrano"
-
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
